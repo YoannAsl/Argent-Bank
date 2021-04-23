fb5b5ec304e9c2f0b38ddc5febfaeb62e1e0967e
YoannAsl <47599801+YoannAsl@users.noreply.github.com>
2021-04-24 00:00:25 +0200

    Added Nav, HomePage and Footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import HomePage from './pages/HomePage';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Nav from './components/Nav/Nav';
+import HomePage from './pages/HomePage/HomePage';
+import Footer from './components/Footer/Footer';
 
 function App() {
-	return (
-		<div className='App'>
-			<HomePage />
-		</div>
-	);
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Nav />
+                <Route exact path="/">
+                    <HomePage />
+                </Route>
+                <Footer />
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
