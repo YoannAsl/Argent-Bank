64171ec1ca20e685db068374fb53b98e9b592c0e
YoannAsl <47599801+YoannAsl@users.noreply.github.com>
2021-04-24 13:23:49 +0200

    Added UserPage and Account

---
+++ src/components/Account/Account.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Account.scss';
+
+export default function Account({ title, amount, description }) {
+	return (
+		<section class='account'>
+			<div class='account-content-wrapper'>
+				<h3 class='account-title'>{title}</h3>
+				<p class='account-amount'>${amount}</p>
+				<p class='account-amount-description'>{description}</p>
+			</div>
+			<div class='account-content-wrapper cta'>
+				<button class='transaction-button'>View transactions</button>
+			</div>
+		</section>
+	);
+}
